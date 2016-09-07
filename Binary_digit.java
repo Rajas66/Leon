8a68b1f69b5c54415a5d9bdb0f841fa05988e37e
Rajas66 <rajchitra6@gmail.com>
2016-09-07 09:45:01 +0530

    Binary_digit.java

---
+++ Binary_digit.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class Binary_digit 
+{
+    static Scanner in=new Scanner(System.in);
+    static int input,val,inc=0,i,ans;
+    static int sta[]=new int[1000];
+    public static int main1(int input,int checking)
+    {
+        ans=0;
+        inc=0;
+        val=3;
+        while(input>0 || val>0)
+        {
+            sta[inc]=input%2;
+            input=input/2;
+            inc++;
+            val--;
+        }
+        
+        ans=sta[inc];
+        if(inc>checking)
+        {
+            return 0;
+        }
+        else
+            if(inc<checking)
+            {
+                return 1;
+            }
+        for(i=inc-1;i>=0;i--)
+        {
+            ans=(ans*10)+sta[i];
+            System.out.print(sta[i]+"");
+        }
+        System.out.println();
+        return 1;
+    }
+    public static void main(String args[])
+    {
+        int asdf;
+        int l=1,limit,check=3,row=0;
+        limit=in.nextInt();
+        for(l=1;l<=999999999;l++)
+        {
+            asdf=main1(l,limit);
+            if(asdf==0)
+            {
+                break;
+            }
+        }
+    }
+}
